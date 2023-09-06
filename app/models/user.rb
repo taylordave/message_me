768589d3dc43b850d85b467aa905509da181cf99
Dave Taylor <ddataylor@gmail.com>
2023-09-06 13:25:58 +0100

    Add User resource with `rails generate model user`, enable bcrypt, enable Hirb, configure model so that username is unique and has constraints and include some seed data

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  before_save { self.username = username.downcase }
+  validates :username,
+            presence: true,
+            uniqueness: { case_sensitive: false },
+            length: { minimum: 3, maximum: 15 }
+  has_secure_password
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "Mashrur", password: "password")
+User.create(username: "Evgeny", password: "password")
+User.create(username: "Arya", password: "password")
+User.create(username: "Fredo", password: "password")
+User.create(username: "Gandalf", password: "password")
