df092a0c8366c458c61a54e8a8e4e2cd0d0f8b02
Dave Taylor <ddataylor@gmail.com>
2023-09-07 21:24:52 +0100

    Setup scrolling so that it shows the latest messages

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,14 +17,22 @@
 //= require semantic-ui
 //= require_tree .
 
-$(document).on('turbolinks:load', function() {
+scroll_bottom = function () {
+    let $messages = $('#messages')
+    if ($messages.length > 0) {
+        $messages.scrollTop($messages[0].scrollHeight);
+    }
+}
+
+$(document).on('turbolinks:load', function () {
     $('.ui.dropdown').dropdown();
     $('.message .close')
-        .on('click', function() {
+        .on('click', function () {
             $(this)
                 .closest('.message')
                 .transition('fade')
             ;
         })
     ;
+    scroll_bottom();
 })
